a6377f57e5568e71b3c24c474b155f8c97bee4b5
1220662994@qq.com <1220662994@qq.com>
2015-01-12 21:31:24 +0800

    2015-01-12 Helen 更新db文件

--- sechand-platform/WebRoot/db/sechand_db.sql
+++ sechand-platform/WebRoot/db/sechand_db.sql
@@ -1,26 +1,19 @@
-﻿/*
+/*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50151
-Source Host           : localhost:3306
+Source Server         : localhost
+Source Server Version : 50529
+Source Host           : 127.0.0.1:3306
 Source Database       : sechand_db
 
 Target Server Type    : MYSQL
-Target Server Version : 50151
+Target Server Version : 50529
 File Encoding         : 65001
 
-Date: 2015-01-09 12:44:27
+Date: 2015-01-12 21:30:04
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- CREATE DATABASE sechand_db
--- ----------------------------
-
--- DROP DATABASE IF EXISTS sechand_db;
--- CREATE DATABASE  sechand_db DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- ----------------------------
 -- Table structure for tb_account
@@ -28,7 +21,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `tb_account`;
 CREATE TABLE `tb_account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_code` int(11) NOT NULL,
+  `role_code` varchar(45) NOT NULL,
   `role_name` varchar(45) NOT NULL,
   `user_name` varchar(45) NOT NULL,
   `real_name` varchar(45) DEFAULT NULL,
@@ -46,7 +39,7 @@ CREATE TABLE `tb_account` (
 -- ----------------------------
 -- Records of tb_account
 -- ----------------------------
-INSERT INTO `tb_account` VALUES ('1', '1', '管理员', 'admin', null, 'Eeoc9PntH9npT0UZY8kDZQ==', '管理员', null, null, '手动录入', '2015-01-09 12:41:18', '2015-01-09 12:40:38', '正常');
+INSERT INTO `tb_account` VALUES ('1', '1', '管理员', 'admin', null, 'Eeoc9PntH9npT0UZY8kDZQ==', 'Helen', null, null, '手动录入', null, '2015-01-12 21：27：28', '正常');
 
 -- ----------------------------
 -- Table structure for tb_order
@@ -73,7 +66,7 @@ DROP TABLE IF EXISTS `tb_role`;
 CREATE TABLE `tb_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `code` int(11) NOT NULL,
+  `code` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
